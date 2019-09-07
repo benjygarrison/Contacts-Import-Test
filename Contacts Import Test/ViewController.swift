7ff365215137aca633d70945cf114b8a4a73f37f
benjyGarrison <benjygarrison@gmail.com>
2019-09-07 14:44:06 -0700

    Added contact selection function; need to import contact viewer code from other project

--- Contacts Import Test/ViewController.swift
+++ Contacts Import Test/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import ContactsUI
+class ViewController: UIViewController,CNContactPickerDelegate{
 
-class ViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//add function below to existing code in main PASSWORD project (import from other project)
+    
+        //MARK:- contact picker
+        func onClickPickContact(){
+            
+            
+            let contactPicker = CNContactPickerViewController()
+            contactPicker.delegate = self
+            contactPicker.displayedPropertyKeys =
+                [CNContactGivenNameKey
+                    , CNContactPhoneNumbersKey]
+            self.present(contactPicker, animated: true, completion: nil)
+            
+        }
+        
+        func contactPicker(_ picker: CNContactPickerViewController,
+                           didSelect contactProperty: CNContactProperty) {
+            
+        }
+        
+        func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+            // You can fetch selected name and number in the following way
+            
+            // user name
+            let userName:String = contact.givenName
+            
+            // user phone number
+            let userPhoneNumbers:[CNLabeledValue<CNPhoneNumber>] = contact.phoneNumbers
+            let firstPhoneNumber:CNPhoneNumber = userPhoneNumbers[0].value
+            
+            
+            // user phone number string
+            let primaryPhoneNumberStr:String = firstPhoneNumber.stringValue
+            
+            print(primaryPhoneNumberStr)
+            
+        }
+        
+        func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
+            
+        }
     }
-
-
-}
 
